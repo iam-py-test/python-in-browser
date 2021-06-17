15cd26040e00a0d6ee17d26e1268d0a248e7926f
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-17 10:00:08 -0400

    Update py-parse.js

--- py-parse.js
+++ py-parse.js
@@ -1,1 +1,8 @@
+var main = async function(){
+}
+window.jsload = true
+main().catch(function(err){
+  console.log("Error:",err);
+  main().catch(function(){alert("Error occured. Please reload")})
+                          })
 
