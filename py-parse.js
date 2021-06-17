866f2539ada24b1c5fdb1fe34b8c47710ff83bfe
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-17 10:16:19 -0400

    Update py-parse.js

--- py-parse.js
+++ py-parse.js
@@ -2,10 +2,10 @@ var main = async function(){
   document.getElementById('writepy').textContent = "print(\"Hi\")"
   document.getElementById("output").textContent = "> Python"
   document.getElementById("run").onclick = async function(){
-    var text = document.getElementById("writepy").textContent
+    var text = document.getElementById("writepy").innerText
     var splittext = text.split("\n")
     for(var t = 0;t < splittext.length;t++){
-      console.log(t,splittext[t],splittext)
+      console.log(t,splittext[t])
       if(splittext[t].startsWith("print(\"")){
          document.getElementById("output").innerText += "\n" + splittext[t].replace("print(\"","").slice(0,-2)
     }
